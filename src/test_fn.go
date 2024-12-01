3becbc7d6fc41318601e6508217e131a5bf98f74
Vibhanshu Prasad <vibhansuprasad@gmail.com>
2024-12-01 11:46:56 +0530

    comply with go project structure

---
+++ src/test_fn.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	fmt "fmt"
+    "os"
+    "Packages/lib"
+)
+
+func main() {
+	fmt.Println("test function calls")
+	fn(1)
+	defer fn(2)
+	fn(3)
+	defer fn(4)
+	fn(5)
+	defer fn(6)
+    fmt.Println("--- ARGS ---- ")
+    fmt.Println(os.Args)
+    fmt.Println(len(os.Args))
+    fmt.Println("--- ARGS ---- ")
+    val := new(int)
+    for i := 0; i != 10; i++ {
+        incr_var(val)
+        fmt.Println(*val)
+    }
+    var s string
+    fmt.Println(s)
+    s = "This is test string"
+    fmt.Println(s)
+}
---
+++ vib/lib.go
@@ -0,0 +1,11 @@
+
+package lib
+
+func fn(v int) {
+	fmt.Println("value of variable: ", v)
+}
+
+func incr_var(v *int) {
+    *v += 100
+}
+
