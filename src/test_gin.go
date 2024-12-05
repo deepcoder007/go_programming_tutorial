b5641c73270557659c23b56d93095ee1ed0d7b7a
vibhanshu prasad <vibhanshuprasad@gmail.com>
2024-12-05 08:14:24 +0530

    gin example

---
+++ src/test_gin.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to GIN!",
+		})
+	})
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello " + name,
+		})
+	})
+
+	router.POST("/user", func(c *gin.Context) {
+		var json struct {
+			Name string `json:"name" binding:"required"`
+			Age int `json:"age" binding:"required"`
+		}
+
+		if err := c.ShoulBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "user created",
+			"user": json,
+		})
+	})
+
+	router.Run(":8080")
+}
