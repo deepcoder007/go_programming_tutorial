10140ebd461787e912bfa5cb070fe73e20249b10
Vibhanshu Prasad <vibhansuprasad@gmail.com>
2024-12-19 05:06:08 +0530

    misc changes

--- src/redis_pub_stream.go
+++ src/redis_pub_stream.go
@@ -18,7 +18,7 @@ func main() {
 
 	{
 		chan_name := "test_channel"
-		// chan_name_2 := "second_channel"
+		chan_name_2 := "second_channel"
 
 		for i := 0; i != 10; i++ {
 			err := client.Publish(ctx, chan_name, "message1").Err()
@@ -26,41 +26,11 @@ func main() {
 				fmt.Println("unable to publish")
 				log.Fatalf("Failed to subscribe: %v", err)
 			}
+			err = client.Publish(ctx, chan_name_2, "second_message").Err()
+			if err != nil {
+				fmt.Println("unable to publish")
+				log.Fatalf("Failed to subscribe: %v", err)
+			}
 		}
-
-		// pubsub := client.Subscribe(ctx, chan_name)
-		// // pubsub := client.Subscribe(ctx, chan_name, chan_name_2)
-		// pubsub_2 := client.Subscribe(ctx, chan_name_2)
-		// defer pubsub.Close()
-		// defer pubsub_2.Close()
-
-		// _, err := pubsub.Receive(ctx)
-		// if err != nil {
-		// 	log.Fatalf("Failed to subscribe: %v", err)
-		// }
-		// fmt.Printf("Subscribed to channel: %s\n", chan_name)
-		// _, err = pubsub_2.Receive(ctx)
-		// if err != nil {
-		// 	log.Fatalf("Failed ot subscribe: %v", err)
-		// }
-		// fmt.Printf("Subscribed to channel: %s\n", chan_name_2)
-
-		// ch := pubsub.Channel()
-		// ch_2 := pubsub_2.Channel()
-		// fmt.Println(ch)
-		// fmt.Println(ch_2)
-		// // for msg := range ch {
-		// // 	fmt.Printf("Received message from  %s: %s\n", msg.Channel, msg.Payload)
-		// // }
-		// for {
-		// 	select {
-		// 		case msg := <-ch: {
-		// 			fmt.Printf("Received message from  %s: %s\n", msg.Channel, msg.Payload)
-		// 		}
-		// 		case msg := <-ch_2: {
-		// 			fmt.Printf("Received message from  %s: %s\n", msg.Channel, msg.Payload)
-		// 		}
-		// 	}
-		// }
 	}
 }
